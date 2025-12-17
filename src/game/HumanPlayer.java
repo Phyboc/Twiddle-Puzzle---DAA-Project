c296f809f2e6c2f2e521d421a80223cbe3085206
Sivasubramani K J <sivasubramanikj@gmail.com>
2025-12-17 13:27:13 +0530

    Refactor HumanPlayer to extend AbstractPlayer
    Refactor HumanPlayer class to extend AbstractPlayer and handle user input for moves.

--- src/game/HumanPlayer.java
+++ src/game/HumanPlayer.java
@@ -1,32 +1,24 @@
 package game;
 import java.util.*;
 
-public class GameEngine {
-    private final Board board;
-    private final Player human;
-    private final Player computer;
+public class HumanPlayer extends AbstractPlayer {
 
-    public GameEngine(Board board, Player human, Player computer) {
-        this.board = board;
-        this.human = human;
-        this.computer = computer;
+    private final Scanner sc = new Scanner(System.in);
+
+    public HumanPlayer(Board board) {
+        super(board);
     }
+    public int getMove() {
+        System.out.print("\nEnter your move number (1-4): ");
+        int mv = sc.nextInt();
 
-    public void startGame() {
-        System.out.println("Starting Puzzle...\n");
-
-        board.print();
-
-        while (!board.isSolved()) {
-
-            System.out.println("*** Human's turn ***");
-            board.executeMove(human.getMove());
-            if (board.isSolved()) break;
-
-            System.out.println("*** Computer's turn ***");
-            board.executeMove(computer.getMove());
+        while (mv < 1 || mv > 4) {
+            System.out.print("Invalid move. Enter again (1-4): ");
+            mv = sc.nextInt();
         }
-
-        System.out.println("🎉 Puzzle Solved!");
+        return mv;
+    }
+    public String getName() {
+        return "Human";
     }
 }
